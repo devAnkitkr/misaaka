a0bc37f0b55546fbf784574403882eafc5b7af7c
Ankit kumar <itsmeankit12feb@gmail.com>
2022-03-24 00:40:01 +0530

    added shadow on features banner

--- components/FeatureBanner.js
+++ components/FeatureBanner.js
@@ -3,22 +3,22 @@ import { SvgIcons } from '../utils/svgIcons';
 
 export default function FeatureBanner() {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center my-10">
-      <div className="border rounded-lg w-full h-[150px] mx-4 my-4 flex justify-center items-center">
+    <div className="flex flex-col md:flex-row justify-between items-center my-10 p-4 md:p-0">
+      <div className="border rounded-lg w-full h-[150px] mx-4 my-4 flex justify-center items-center bg-white drop-shadow">
         <div className="p-4 fill-teal-400">{SvgIcons.shippingIcon}</div>
         <div className="flex flex-col">
           <h1 className="text-heading text-xl">Free Shipping</h1>
           <p className="text-caption">All orders over Rs. 299</p>
         </div>
       </div>
-      <div className="border rounded-lg w-full h-[150px] mx-4 my-4 flex justify-center items-center">
+      <div className="border rounded-lg w-full h-[150px] mx-4 my-4 flex justify-center items-center bg-white drop-shadow">
         <div className="p-4 fill-teal-400">{SvgIcons.paymentIcon}</div>
         <div className="flex flex-col">
           <h1 className="text-heading text-xl">Secured Payment</h1>
           <p className="text-caption">Support 24/7</p>
         </div>
       </div>
-      <div className="border rounded-lg w-full h-[150px] mx-4 my-4 flex justify-center items-center">
+      <div className="border rounded-lg w-full h-[150px] mx-4 my-4 flex justify-center items-center bg-white drop-shadow">
         <div className="p-4 fill-teal-400">{SvgIcons.returnIcon}</div>
         <div className="flex flex-col">
           <h1 className="text-heading text-xl">14-Day Return</h1>
