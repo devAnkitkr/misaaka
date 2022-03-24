ccc01d12eaab9d11b5600825d9dffa3573fdc958
Ankit kumar <itsmeankit12feb@gmail.com>
2022-03-25 01:56:23 +0530

    cover slider edited with caption

--- components/CoverSlider.js
+++ components/CoverSlider.js
@@ -2,16 +2,18 @@ import React from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const CoverSlider = () => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const router = useRouter();
 
   return (
-    <div className="embla overflow-hidden mt-4 mx-4" ref={emblaRef}>
+    <div className="embla overflow-hidden mt-4 mx-4 relative" ref={emblaRef}>
       <div className="embla_container flex">
         <div className="embla__slide relative" style={{ flex: '0 0 100%' }}>
           <Image
-            src="/static/banner1.jpg"
+            src="/static/banner6.jpg"
             width="900"
             height="500"
             layout="responsive"
@@ -38,6 +40,21 @@ const CoverSlider = () => {
             objectFit="cover"
           />
         </div>
+      </div>
+      <div className="w-full h-full absolute top-0 left-0 bg-zinc-900/20 flex flex-col justify-center items-center text-white font-bold text-lg md:text-2xl">
+        <span className="text-center mb-2 md:mb-4">
+          COME SEE <br />
+          WHAT YOU LOVE!
+        </span>
+        <span className="text-base font-normal">
+          Crockery | Stoneware | Ceramics | Decor
+        </span>
+        <button
+          className="rounded bg-rose-400 px-5 md:px-10 py-2 my-2 mt-8 text-white font-normal text-base md:text-lg font-bold hover:bg-rose-500 transition-[bg-color] ease-in duration-150"
+          onClick={() => router.push('/shop')}
+        >
+          SHOP NOW!
+        </button>
       </div>
     </div>
   );
